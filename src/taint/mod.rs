5daea5b18833ae3f9b72f3a05075a33df7582800
Niko Lepka <slench102+git@gmail.com>
2018-09-27 12:22:15 +0200

    add additional documentation

--- src/taint/mod.rs
+++ src/taint/mod.rs
@@ -1,8 +1,8 @@
 // IMPORTANT! Everything general about the library goes here.
 //            Everything else goes into the other folders like clean and dirty.
 
-pub mod clean;
-pub mod dirty;
+mod clean;
+mod dirty;
 
 // re-exports Clean and Dirty, so users don't have to write `use stainless::taint::clean::Clean`, 
 // but instead just `use stainless::taint::Clean` (and similar for Dirty)
